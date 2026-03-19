dcd5ea3f94092ba83bb558f0e608564b66954d44
Jeffry Gonzalez <jeff@hypertheory.com>
2026-03-18 21:40:15 -0400

    fixed list

--- src/app/areas/resources/resources-landing/internal/pages/list.ts
+++ src/app/areas/resources/resources-landing/internal/pages/list.ts
@@ -1,13 +1,12 @@
 import { httpResource } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { PageLayout } from '@ht/shared/ui-common/layouts/page';
-import { ResourceApiItemModel } from '../types';
 import { ExtractHostPipe } from '../../../util-pipes/extract-host';
-import { TitleCasePipe } from '@angular/common';
+import { ResourceApiItemModel } from '../types';
 
 @Component({
   selector: 'app-resources-pages-list',
-  imports: [PageLayout, ExtractHostPipe, TitleCasePipe],
+  imports: [PageLayout, ExtractHostPipe],
   template: `<app-ui-page title="Developer Resource List">
     @if (linksResource.isLoading()) {
       <span class="loading loading-spinner text-primary"></span>
@@ -18,7 +17,7 @@ import { TitleCasePipe } from '@angular/common';
             <div class="card-body">
               <h2 class="card-title text-xl text-accent">{{ link.title }}</h2>
               <a class="btn btn-primary btn-xs" [href]="link.url" target="_blank"
-                >Visit {{ link.url | extractHost | titlecase }}</a
+                >Visit {{ link.url | extractHost: true }}</a
               >
             </div>
           </div>
